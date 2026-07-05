87ca16afd0caec1348041631f3c8116491ea7e6b
Artur Sannikov <git-sign@asannikov.com>
2026-07-05 13:25:35 +0300

    feat: add ucs

--- default.nix
+++ default.nix
@@ -7,7 +7,9 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 {
   # The `lib`, `overlays`, `nixosModules`, `homeModules`,
@@ -19,7 +21,7 @@
   # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
+
+  ucs = pkgs.callPackage ./pkgs/ucs { };
 }
